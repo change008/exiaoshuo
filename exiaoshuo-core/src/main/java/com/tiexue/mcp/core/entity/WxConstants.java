a90290368add15ae52ab149040dc27981925c17c
fangjie008 <fangjie008@163.com>
2017-09-26 23:31:43 +0800

    123

--- exiaoshuo-core/src/main/java/com/tiexue/mcp/core/entity/WxConstants.java
+++ exiaoshuo-core/src/main/java/com/tiexue/mcp/core/entity/WxConstants.java
@@ -2,36 +2,36 @@ package com.tiexue.mcp.core.entity;
 
 public class WxConstants {
 	// 微信验证 token
-	public static final String WxToken = "txmobilekey"; //ok_peng
+	public static final String WxToken = "txmobilekey"; //ok
 
 	// 微信网页授权state
-	public static final String WxOauthState = "ok"; //ok_peng
+	public static final String WxOauthState = "ok"; //ok
 
 	// 微信公众号Appid
-	public static final String WxAppId = "wx859d0437f6f1c2f6"; //ok_peng
+	public static final String WxAppId = "wxc81e9472a78d0785"; //ok
 
 	// 微信公众号AppSecret
-	public static final String WxAppSecret = "ec9bae84c4eafd0f08af3353ff02afc6"; //ok_peng
+	public static final String WxAppSecret = "c1354285cda071be329ffae80c4806d9"; //ok
 
 	// 微信网页授权回调地址
-	public static final String WxRedirectUrl = "http://xs.xiaohongwei.cn/wxUser/wxoauthcallback"; //ok_peng
+	public static final String WxRedirectUrl = "http://du.xiaohongwei.cn/wxUser/wxoauthcallback"; //ok
 
 	// 微信网页授权语言编码
-	public static final String WxSnsLang = "zh_CN"; //ok_peng
+	public static final String WxSnsLang = "zh_CN"; //ok
 
 	// 微信商户支付相关
-	public static final String WxMch_Id = "1487939472"; //ok_peng
+	public static final String WxMch_Id = "1489679572"; //ok
 
 	// 微信商户支付秘钥
-	public static final String WxMch_Key = "t0EZVIvBwvytVL4RYh2H0HuR6sygnDxv"; //ok_peng
+	public static final String WxMch_Key = "t0EZVIvBwvytVL4RYh2H0HuR6sygnDxv"; //ok
 
 	// 微信商户支付后回调url
-	public static final String WxMch_NotifyUrl = "http://xs.xiaohongwei.cn/wxPay/wxpaycallback"; //ok_peng
+	public static final String WxMch_NotifyUrl = "http://du.xiaohongwei.cn/wxPay/wxpaycallback"; //ok
 
 	// 签名方式
-	public static final String WxMch_SignType = "MD5";  //ok_peng
+	public static final String WxMch_SignType = "MD5";  //ok
 
 	// 交易发起方式
-	public static final String WxMch_TradeType = "JSAPI";  //ok_peng
+	public static final String WxMch_TradeType = "JSAPI";  //ok
 
 }
