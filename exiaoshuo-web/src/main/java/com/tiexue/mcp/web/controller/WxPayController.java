8ca6e4e01c4c37b2576055e3b61bf4aa532b08e2
fangjie008 <fangjie008@163.com>
2017-10-19 23:36:51 +0800

    123

--- exiaoshuo-web/src/main/java/com/tiexue/mcp/web/controller/WxPayController.java
+++ exiaoshuo-web/src/main/java/com/tiexue/mcp/web/controller/WxPayController.java
@@ -144,6 +144,9 @@ public class WxPayController {
 		request.setAttribute("chapterid", chapterid);
 		request.setAttribute("fromurl", fm);
 		String refer = request.getHeader("Referer");
+		
+		logger.error("******************pay_1 refer:" + refer + "**********************");
+		
 		if (null != refer && !refer.isEmpty()) {
 			Cookie _refCookie = new Cookie("_refpay", refer); // 创建一个Cookie对象，并将用户名保存到Cookie对象中
 			_refCookie.setMaxAge(10*60); // 设置Cookie的过期之前的时间，单位为秒
@@ -281,15 +284,18 @@ public class WxPayController {
 			logger.error("fromurl:"+fm);
 			attr.addAttribute("fm", fm);
 			// todo:在这里处理支付成功的逻辑,跳转到原来阅读地址,或者到首页等
+			
+			logger.error("******************payresult_1 ref:" + ref + "**********************");
+			
 			if (ref != ""&&(ref.contains("myzhangjiecontent")||ref.contains("myuser")||ref.contains("myzhangjie")||ref.contains("myxiaofei"))) {
-				logger.error("pay _refpay:"+ref);
+				logger.error("******************payresult_2 ref:" + ref + "**********************");
 				Cookie _refCookie = new Cookie("_refpay", ""); // 创建一个Cookie对象，并将用户名保存到Cookie对象中
-				_refCookie.setMaxAge(5*60); // 设置Cookie的过期之前的时间，单位为秒
+				_refCookie.setMaxAge(10*60); // 设置Cookie的过期之前的时间，单位为秒
 				response.addCookie(_refCookie); // 通过response的addCookie()方法将此Cookie对象保存到客户端的Cookie中
 				
 				return "redirect:" + ref;
 			}
-			logger.error("pay _refpay:"+ref);
+			logger.error("******************payresult_3 ref:" + ref + "**********************");
 			return "redirect:/mynovel/index";
 		} catch (Exception e) {
 			logger.error("支付成功后跳转报错：" + e.getMessage());
